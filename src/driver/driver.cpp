b6eec27a767891a41a04c1781c55083f135030fc
Phillip Johnston <phillip@embeddedartistry.com>
2021-08-09 16:17:48 -0700

    Start placing items in the .cpp file

--- src/driver/driver.cpp
+++ src/driver/driver.cpp
@@ -1,6 +1,15 @@
 #include <driver.hpp>
 
-int ret42(void)
+// TODO: support fixed point and floating point
+// TODO: is this another secret we can hide?
+
+#pragma mark - Definitions -
+
+enum sps30_device_status_mask_t
 {
-	return 42;
-}
+	FAN_ERROR = (1 << 4),
+	LASER_ERROR = (1 << 5),
+	FAN_SPEED_WARNING = (1 << 21)
+};
+
+
