1ecd3331ca60ae3bddb3d5fb67510cedc2ce4be3
Phillip Johnston <phillip@embeddedartistry.com>
2021-08-22 10:02:30 -0700

    Checkpoint - define transport API, prepare for templating, and eliminate vendor placeholder.

--- src/driver/driver.hpp
+++ src/driver/driver.hpp
@@ -10,10 +10,27 @@
 #include <cstddef>
 #include <cstdint>
 
-// For now, we want to develop this class
-// for I2C. Eventually we will refactor it so we can
-// configure the specific transport (and it's necessary
-// function pointers) on boot
+namespace sps30
+{
+/// The delay between issuing a SPS30Sensor::reset() call and attempting to resume measurements
+static constexpr std::chrono::duration<uint32_t, std::micro> RESET_DELAY_USEC =
+	std::chrono::microseconds(100000);
+/// The interval between measurements must be at least this duration
+/// Datasheet specifies 1±0.04s
+static constexpr std::chrono::duration<uint32_t, std::micro> MINIMUM_MEASUREMENT_DURATION_USEC =
+	std::chrono::seconds(1);
+/// Time between starting measurement and stable number concentration readings in the 200-3000
+/// #/cm^3 range
+static constexpr std::chrono::duration<uint32_t, std::micro>
+	NUM_CONCENTRATION_3000_200_SETTLING_TIME_USEC = std::chrono::seconds(8);
+/// Time between starting measurement and stable number concentration readings in the 100-200 #/cm^3
+/// range
+static constexpr std::chrono::duration<uint32_t, std::micro>
+	NUM_CONCENTRATION_200_100_SETTLING_TIME_USEC = std::chrono::seconds(8);
+/// Time between starting measurement and stable number concentration readings in the 50-100 #/cm^3
+/// range
+static constexpr std::chrono::duration<uint32_t, std::micro>
+	NUM_CONCENTRATION_100_50_SETTLING_TIME_USEC = std::chrono::seconds(8);
 
 // TODO: a secret we can control is whether or not the device is powered.
 // Is this a separate abstraction? sps_30_power_control
@@ -55,11 +72,44 @@
  * We work around this by reading the value on probe(), and then we cache
  * any values that are set manually.
  */
-class SPS30Sensor
+class sensor
 {
+  private:
+#if 0
+SPEED: Fan speed out of range
+0: Fan speed is ok.
+1: Fan speed is too high or too low.
+During the first 3 seconds after starting the measurement (fan start-up) the fan speed is not checked.
+The fan speed is also not checked during the auto cleaning procedure.
+Apart from the two exceptions mentioned above, the fan speed is checked once per second in the measurement mode. If it is out of range twice in succession, the SPEED-bit is set.
+At very high or low ambient temperatures, the fan may take longer to reach its target speed after start-up. In this case, the bit will be set. As soon as the target speed is reached, this bit is cleared automatically.
+If this bit is constantly set, this indicates a problem with the power supply or that the fan is no longer working properly.
+LASER: Laser failure
+0: Laser current is ok.
+1: Laser is switched on and current is out of range.
+The laser current is checked once per second in the measurement mode. If it is out of range twice in succession, the LASER-bit is set.
+If the laser current is back within limits, this bit will be cleared automatically.
+A laser failure can occur at very high temperatures outside of specifications or when the laser module is defective.
+FAN: Fan failure, fan is mechanically blocked or broken.
+0: Fan works as expected.
+1: Fan is switched on, but the measured fan speed is 0 RPM.
+The fan is checked once per second in the measurement mode. If 0 RPM is measured twice in succession, the FAN bit is set.
+The FAN-bit will not be cleared automatically.
+A fan failure can occur if the fan is mechanically blocked or broken.
+#endif
+	enum device_status_mask_t
+	{
+		/// The fan is switched on but not running
+		FAN_ERROR = (1 << 4),
+		/// The laser is currently out of range
+		LASER_ERROR = (1 << 5),
+		/// The fan speed is out of range
+		FAN_SPEED_WARNING = (1 << 21)
+	};
+
   public:
 	/// Version format for the SPS-30 Sensor Version
-	struct sps30_version_t
+	struct version_t
 	{
 		uint8_t major;
 		uint8_t minor;
@@ -158,6 +208,8 @@ class SPS30Sensor
 	 * Initializes the SPS-30 and places it into an operational state, where it is
 	 * taking measurements
 	 *
+	 * TODO: does this just call probe()? then we eliminate the probe public interface?
+	 *
 	 * @post Device is initialized and taking measurements.
 	 *
 	 * @note Once the driver is started, measurements are retrievable once per second
@@ -224,11 +276,11 @@ class SPS30Sensor
 	 *
 	 * @pre Sensor has been probed.
 	 *
-	 * @returns An sps30_version_t structure instance containing the major and minor version
+	 * @returns An sps30::version_t structure instance containing the major and minor version
 	 * reported by firmware.
 	 *
 	 */
-	sps30_version_t firmwareVersion();
+	version_t firmwareVersion();
 
 	/** Retrieve the sensor's serial number
 	 *
@@ -296,6 +348,35 @@ class SPS30Sensor
 	void cleanFan();
 
   private:
+#if 0
+	/** Read the Device Status Register
+	 *
+	 * Reads the Device Status Registerm which reveals info, warnings and errors
+	 * about the sensor's current operational state. Note that the flags are
+	 * self-clearing, i.e. they reset to 0 when the condition is resolved.
+	 *
+	 * @note This command only works on firmware 2.2 or more recent.
+	 *
+	 * @param [out] device_status_flags Memory where the register value will be written to
+	 *
+	 * @returns status_t::OK on success, or an error indicating the source of the failure
+	 */
+	status_t readStatusRegister(uint32_t& device_status_flags);
+#endif
+
+#if 0
+6.3.1 Start Measurement (0x0010)
+Starts the measurement. After power up, the module is in Idle-Mode. Before any measurement values can be read, the Measurement-Mode needs to be started using this command.
+Transfer Type: Set Pointer & Write Data Pointer Address: 0x0010
+Write Data:
+0 Measurement Output Format
+0x03: Big-endian IEEE754 float values
+0x05: Big-endian unsigned 16-bit integer values
+1 dummy byte, insert 0x00
+2 Checksum for bytes 0, 1
+#endif
+
+  private:
 	bool started_ = false;
 	bool probed_ = false;
 	/// Fan auto-clean interval
@@ -303,5 +384,7 @@ class SPS30Sensor
 	sps30_version_t version_;
 	char serial_[SPS30_SERIAL_NUM_BUFFER_LEN];
 };
+
+}; // end namespace sps30
 
 #endif // SPS_30_DRIVER_HPP_
--- src/driver/sps30_transport.hpp
+++ src/driver/sps30_transport.hpp
@@ -1,38 +1,74 @@
+#ifndef SPS30_TRANSPORT_INTERFACE_HPP_
+#define SPS30_TRANSPORT_INTERFACE_HPP_
 
+#include <stdint.h>
 
-/// Possible transport function status return values
-enum sps30_transport_status_t
+// TODO: refactor this class for being asynchronous
+// TODO: ultimately use CRTP here?
+// https://www.fluentcpp.com/2017/05/12/curiously-recurring-template-pattern/ OR:
+// https://www.fluentcpp.com/2020/09/11/replacing-crtp-static-polymorphism-with-concepts/
+class SPS30Transport
 {
-	/// Operation was successful
-	OK = 0,
-	/// An error occurred while transmitting or receiving data on the hardware bus
-	BUS_ERROR,
-	/// The driver's state is not valid for this operation
-	/// E.g., trying to call read() when the driver that has not been started
-	INVALID_STATE,
-	/// The device firmware does not support the specified command
-	SENSOR_FIRWMARE_DOES_NOT_SUPPORT_COMMAND,
-	/// The action you are attempting is not currently implemented
-	NOT_IMPLEMENTED,
-	/// An error occurred, but its details are not specified
-	UNKNOWN_ERROR,
+  public:
+	/// Possible transport function status return values
+	enum status_t
+	{
+		/// Operation was successful
+		OK = 0,
+		/// An error occurred while transmitting or receiving data on the hardware bus
+		BUS_ERROR,
+		/// The driver's state is not valid for this operation
+		/// E.g., trying to call read() when the driver that has not been started
+		INVALID_STATE,
+		/// The device firmware does not support the specified command
+		SENSOR_FIRWMARE_DOES_NOT_SUPPORT_COMMAND,
+		/// The action you are attempting is not currently implemented
+		NOT_IMPLEMENTED,
+		/// An error occurred, but its details are not specified
+		UNKNOWN_ERROR,
+	};
+
+	/// Generic representation of commands that can be issued across the SPS-30
+	/// transport layer.
+	enum command_t
+	{
+		SPS30_CMD_START_MEASUREMENT = 0,
+		SPS30_CMD_START_MEASUREMENT_ARG,
+		SPS30_CMD_STOP_MEASUREMENT,
+		SPS30_CMD_READ_MEASUREMENT,
+		SPS30_CMD_GET_DATA_READY,
+		SPS30_CMD_AUTOCLEAN_INTERVAL,
+		SPS30_CMD_GET_FIRMWARE_VERSION,
+		SPS30_CMD_GET_SERIAL,
+		SPS30_CMD_RESET,
+		SPS30_CMD_SLEEP,
+		SPS30_CMD_READ_DEVICE_STATUS_REG,
+		SPS30_CMD_START_MANUAL_FAN_CLEANING,
+		SPS30_CMD_WAKE_UP,
+	};
+
+  public:
+	/** Read data over the transport
+	 *
+	 * @param [out] data Pointer to the buffer where the data should be stored
+	 * @param []
+	 *
+	 * @returns a status_t value indiating the state of the transfer
+	 */
+	status_t read(uint8_t* const data, const size_t length);
+
+	/** Write data over the transport
+	 *
+	 * @returns a status_t value indiating the state of the transfer
+	 */
+	status_t write(const command_t command, const uint8_t* const data, const size_t length);
+
+	/** Send and recieve data over the transport
+	 *
+	 * @returns a status_t value indiating the state of the transfer
+	 */
+	status_t transcieve(const command_t command, const uint8_t* const tx_data,
+						const size_t tx_length, uint8_t* const rx_data, const size_t rx_length);
 };
 
-/// Generic representation of commands that can be issued across the SPS-30
-/// transport layer.
-enum sps30_transport_commands_t
-{
-	SPS30_CMD_START_MEASUREMENT = 0,
-	SPS30_CMD_START_MEASUREMENT_ARG,
-	SPS30_CMD_STOP_MEASUREMENT,
-	SPS30_CMD_READ_MEASUREMENT,
-	SPS30_CMD_GET_DATA_READY,
-	SPS30_CMD_AUTOCLEAN_INTERVAL,
-	SPS30_CMD_GET_FIRMWARE_VERSION,
-	SPS30_CMD_GET_SERIAL,
-	SPS30_CMD_RESET,
-	SPS30_CMD_SLEEP,
-	SPS30_CMD_READ_DEVICE_STATUS_REG,
-	SPS30_CMD_START_MANUAL_FAN_CLEANING,
-	SPS30_CMD_WAKE_UP,
-};
+#endif // SPS30_TRANSPORT_INTERFACE_HPP_
