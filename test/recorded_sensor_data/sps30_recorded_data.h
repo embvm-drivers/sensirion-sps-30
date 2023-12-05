19d2bae1af45aae56ef634bb4f0949caddfe1e2d
Phillip Johnston <phillip@embeddedartistry.com>
2023-12-05 13:24:51 -0800

    Initial setup of recorded data - still needs some more work

---
+++ test/recorded_sensor_data/sps30_recorded_data.h
@@ -0,0 +1,17 @@
+#ifndef SPS30_RECORDED_DATA_H
+#define SPS30_RECORDED_DATA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+extern const uint8_t sps30_request_data_command[2];
+extern const uint8_t sps30_measurement_low_particle_response_1[60];
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // SPS30_RECORDED_DATA_H
