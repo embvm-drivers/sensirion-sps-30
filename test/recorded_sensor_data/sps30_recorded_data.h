b94a13e3772e4e491695c3effd292d4172802d1d
Phillip Johnston <phillip@embeddedartistry.com>
2023-12-06 12:56:34 -0800

    Expand recorded data implementation with better points of comparison as well.

--- test/recorded_sensor_data/sps30_recorded_data.h
+++ test/recorded_sensor_data/sps30_recorded_data.h
@@ -7,11 +7,25 @@ extern "C"
 
 #include <stdint.h>
 
+	/// Recorded I2C command to request device wakeup.
 	extern const uint8_t sps30_wakeup_command[2];
+
+	/// Recorded I2C command to request the firwmare version.
 	extern const uint8_t sps30_request_fw_ver[2];
+	/// Response payload with version 2.2
 	extern const uint8_t sps30_fw_ver_response[3];
+#define SPS30_FW_VER_RESPONSE_MAJOR 2
+#define SPS30_FW_VER_RESPONSE_MINOR 2
+
+	/// Recorded I2C command to request the serial number.
 	extern const uint8_t sps30_request_serial_number[2];
+	/// This is an example recorded response data set
+	/// for the serial number request
 	extern const uint8_t sps30_serial_number_response[48];
+	/// This variable holds the properly decoded serial number for the
+	/// response above and can be used for comparisons.
+	extern const char sps30_serial_number_response_string[17];
+
 	extern const uint8_t sps30_request_data_command[2];
 	extern const uint8_t sps30_measurement_low_particle_response_1[60];
 	extern const uint8_t sps30_measurement_low_particle_response_2[60];
