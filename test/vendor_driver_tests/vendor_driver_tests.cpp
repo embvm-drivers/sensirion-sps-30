6d8d79e7d4bae764e95b4aa3fadde77f7dfdbd68
Phillip Johnston <phillip@embeddedartistry.com>
2023-12-06 13:08:48 -0800

    Expand test coverage

--- test/vendor_driver_tests/vendor_driver_tests.cpp
+++ test/vendor_driver_tests/vendor_driver_tests.cpp
@@ -8,8 +8,7 @@ TEST_CASE("SPS-30 Firmware Version", "[test/vendor_sps30]")
 	uint8_t major_version;
 	uint8_t minor_version;
 
-	// We set the expected TX data that the driver should send
-	// over I2C
+	// We set the expected TX data that the driver should send over I2C
 	sps30_mock_set_i2c_write_data(sps30_request_fw_ver, sizeof(sps30_request_fw_ver));
 	// And then we set the data to be returned by the read command.
 	sps30_mock_set_i2c_read_data(sps30_fw_ver_response, sizeof(sps30_fw_ver_response));
@@ -28,8 +27,7 @@ TEST_CASE("SPS-30 Serial Number", "[test/vendor_sps30]")
 {
 	char serial_num[SPS30_MAX_SERIAL_LEN];
 
-	// We set the expected TX data that the driver should send
-	// over I2C
+	// We set the expected TX data that the driver should send over I2C
 	sps30_mock_set_i2c_write_data(sps30_request_serial_number, sizeof(sps30_request_serial_number));
 	// And then we set the data to be returned by the read command.
 	sps30_mock_set_i2c_read_data(sps30_serial_number_response,
@@ -43,4 +41,26 @@ TEST_CASE("SPS-30 Serial Number", "[test/vendor_sps30]")
 	CHECK(r == 0);
 	CHECK(0 == strncmp(serial_num, sps30_serial_number_response_string,
 					   sizeof(sps30_serial_number_response_string)));
+}
+
+TEST_CASE("SPS-30 Start Manual Fan Cleaning", "[test/vendor_sps30]")
+{
+	// We set the expected TX data that the driver should send over
+	// I2C for this command.
+	sps30_mock_set_i2c_write_data(sps30_request_start_manual_fan_cleaning,
+								  sizeof(sps30_request_start_manual_fan_cleaning));
+
+	auto r = sps30_start_manual_fan_cleaning();
+	CHECK(r == 0);
+}
+
+TEST_CASE("SPS-30 Request Start Measurement", "[test/vendor_sps30]")
+{
+	// We set the expected TX data that the driver should send over
+	// I2C for this command.
+	sps30_mock_set_i2c_write_data(sps30_request_start_measurement,
+								  sizeof(sps30_request_start_measurement));
+
+	auto r = sps30_start_measurement();
+	CHECK(r == 0);
 }
