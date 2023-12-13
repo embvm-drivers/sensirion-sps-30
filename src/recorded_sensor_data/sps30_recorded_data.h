76aba4b17b1c5d4615ef3ba078771ce12e441e1e
Phillip Johnston <phillip@embeddedartistry.com>
2023-12-12 19:10:37 -0800

    Add new recorded data from recent traces

--- src/recorded_sensor_data/sps30_recorded_data.h
+++ src/recorded_sensor_data/sps30_recorded_data.h
@@ -9,6 +9,8 @@ extern "C"
 
 	/// Recorded I2C command to request device wakeup.
 	extern const uint8_t sps30_wakeup_command[2];
+	extern const uint8_t sps30_sleep_command[2];
+	extern const uint8_t sps30_reset_commad[2];
 
 	/// Recorded I2C command to request the firwmare version.
 	extern const uint8_t sps30_request_fw_ver[2];
@@ -16,6 +18,18 @@ extern "C"
 	extern const uint8_t sps30_fw_ver_response[3];
 #define SPS30_FW_VER_RESPONSE_MAJOR 2
 #define SPS30_FW_VER_RESPONSE_MINOR 2
+
+	extern const uint8_t sps30_request_device_status[2];
+	extern const uint8_t sps30_device_status_response_1[6];
+	extern const uint8_t sps30_device_status_response_2[6];
+	extern const uint8_t sps30_request_data_ready[2];
+	extern const uint8_t sps30_data_ready_response_1[3];
+	extern const uint8_t sps30_data_ready_response_2[3];
+	extern const uint8_t sps30_set_fan_auto_cleaning_interval_1[8];
+	extern const uint8_t sps30_set_fan_auto_cleaning_interval_2[8];
+	extern const uint8_t sps30_request_fan_auto_cleaning_interval[2];
+	extern const uint8_t sps30_fan_auto_cleaning_interval_response_1[6];
+	extern const uint8_t sps30_fan_auto_cleaning_interval_response_2[6];
 
 	extern const uint8_t sps30_request_start_manual_fan_cleaning[2];
 
@@ -33,6 +47,10 @@ extern "C"
 	extern const uint8_t sps30_read_measurement_command[2];
 	extern const uint8_t sps30_measurement_low_particle_response_1[60];
 	extern const uint8_t sps30_measurement_low_particle_response_2[60];
+	extern const uint8_t sps30_measurement_low_particle_response_3[60];
+	extern const uint8_t sps30_measurement_mid_particle_response_1[60];
+	extern const uint8_t sps30_measurement_mid_particle_response_2[60];
+	extern const uint8_t sps30_measurement_zero_particle_response[60];
 
 #ifdef __cplusplus
 }
